1ccb29982691643f9dc19dac6d9b104a4b2a827f
Hiren Gondhiya <hirengondhiya@gmail.com>
2017-06-16 21:18:27 +1000

    Converted app to use axios library which uses Promises out of the box.

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,6 +1,6 @@
 const yargs = require('yargs');
-const geocode = require('./geocode/geocode.js');
-const weather = require('./weather/weather.js');
+const axios = require('axios');
+
 const argv = yargs
                 .options({
                     address: {
@@ -13,30 +13,30 @@ const argv = yargs
                 .help()
                 .alias('help', 'h')
                 .argv;
+var encodedAddress = encodeURIComponent(argv.address);
+var geocodeUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`
+axios
+    .get(geocodeUrl)
+    .then((response) => {
+        if (response.data.status === "ZERO_RESULTS") {
+            throw new Error('Unable to find that address.');
+        }
 
-geocode
-    .geocodeAddress(argv.address)
-    .then( (result) => {
-        return weather.getWeather(result.lattitude, result.longitude)
-    }).then((weatherResult) => {
-        console.log(`It is currently: ${weatherResult.temperature}.`);
-        console.log(`It feels like: ${weatherResult.apparentTemperature}.`);
-    }).catch((errorMessage) => {
-        console.log(errorMessage);
+        console.log(response.data.results[0].formatted_address);
+        var lattitude = response.data.results[0].geometry.location.lat;
+        var longitude = response.data.results[0].geometry.location.lng;
+
+        var weatherUrl = `https://api.darksky.net/forecast/0424dacdcb76bdddb4cc4afd4fb1451d/${lattitude},${longitude}`;
+        return axios.get(weatherUrl);
+    }).then((response) => {
+        var temperature = response.data.currently.temperature;
+        var apparentTemperature = response.data.currently.apparentTemperature;
+
+        console.log(`It's currently ${temperature}. It feels like ${apparentTemperature}.`)
+    }).catch((error) => {
+        if (error.code === "ENOTFOUND") {
+            console.log('Unable to connect to API servers.')
+        } else {
+            console.log(error.message);
+        }
     });
-
-// geocode.geocodeAddress(argv.address, (errorMessage, result) => {
-//     if(errorMessage) {
-//         console.log(errorMessage);
-//     } else {
-//         console.log(JSON.stringify(result, undefined, 2));
-//         weather.getWeather(result.lattitude, result.longitude, (weatherErrorMessage, weatherResult) => {
-//             if(weatherErrorMessage) {
-//                 console.log(weatherErrorMessage);
-//             } else {
-//                 console.log(`It is currently: ${weatherResult.temperature}.`);
-//                 console.log(`It feels like: ${weatherResult.apparentTemperature}.`);
-//             }
-//         });
-//     }
-// });
